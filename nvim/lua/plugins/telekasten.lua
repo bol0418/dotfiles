3077db0edf3447070b77b7c3c90b78bc883acdc1
bol0418 <142228386+bol0418@users.noreply.github.com>
2024-05-02 00:25:11 +0900

    refactor: Extract functions to a separate file

--- nvim/lua/plugins/telekasten.lua
+++ nvim/lua/plugins/telekasten.lua
@@ -1,121 +1,109 @@
-return {
-    {
-        "renerocksai/telekasten.nvim",
-        dependencies = {
-            "nvim-telescope/telescope.nvim",
-            "nvim-telescope/telescope-media-files.nvim",
-            "nvim-lua/plenary.nvim",
-            "nvim-lua/popup.nvim",
-            "nvim-telescope/telescope-symbols.nvim",
-            "iamcco/markdown-preview.nvim",
-            "renerocksai/calendar-vim"
-        },
-        config = function()
-            require("telescope").load_extension("media_files")
-            require("telescope").setup({
-                extensions = {
-                    media_files = { "png", "jpg", "jpeg", "webp", "pdf" }
-                }
-            })
+if not vim.g.vscode then
+	return {
+		{
+			"renerocksai/telekasten.nvim",
+			dependencies = {
+				"dharmx/telescope-media.nvim",
+				"iamcco/markdown-preview.nvim",
+				"nvim-lua/plenary.nvim",
+				"nvim-lua/popup.nvim",
+				"nvim-telescope/telescope-symbols.nvim",
+				"nvim-telescope/telescope.nvim",
+				"renerocksai/calendar-vim",
+			},
+			config = function()
+				local home = vim.fn.expand("~/zettelkasten")
+				local templ_dir = vim.fn.stdpath("config") .. "/zettelkasten/templates"
 
-            local home = vim.fn.expand("~/zettelkasten")
-            local templ_dir = vim.fn.stdpath("config") .. "/zettelkasten/templates"
-            require("telekasten").setup({
-                home = home,
-                templates = templ_dir,
-                dailies = home .. "/" .. "daily",
-                extension = ".md",
-                image_link_style = "markdown",
-                subdirs_in_links = true,
-                template_handling = "smart",
-                new_note_location = "smart",
-                rename_update_links = true,
+				require("telekasten").setup({
+					home = home,
+					templates = templ_dir,
+					dailies = home .. "/" .. "daily",
+					extension = ".md",
+					image_link_style = "markdown",
+					subdirs_in_links = true,
+					template_handling = "smart",
+					new_note_location = "smart",
+					rename_update_links = true,
 
-                -- following a link to a non-existing note will create it
-                follow_creates_nonexisting = true,
-                dailies_create_nonexisting = true,
-                weeklies_create_nonexisting = true,
+					-- following a link to a non-existing note will create it
+					follow_creates_nonexisting = true,
+					dailies_create_nonexisting = true,
+					weeklies_create_nonexisting = true,
 
-                -- templates
-                template_new_note = templ_dir .. "/new_note.md",
-                template_new_daily = templ_dir .. "/new_daily.md",
+					-- templates
+					template_new_note = templ_dir .. "/new_note.md",
+					template_new_daily = templ_dir .. "/new_daily.md",
+				})
 
-                require("telescope").load_extension("media_files"),
-            })
+				-- Launch panel if nothing is typed after <leader>z
+				vim.keymap.set(
+					"n",
+					"<leader>z",
+					"<cmd>Telekasten panel<CR>",
+					{ silent = true, desc = "Open Telekasten panel" }
+				)
 
-            -- Launch panel if nothing is typed after <leader>z
-            vim.keymap.set(
-                "n",
-                "<leader>z",
-                "<cmd>Telekasten panel<CR>",
-                { silent = true, desc = "Open Telekasten panel" }
-            )
+				-- Most used functions
+				vim.keymap.set(
+					"n",
+					"<leader>zf",
+					"<cmd>Telekasten find_notes<CR>",
+					{ silent = true, desc = "Find notes using Telescope (telekasten)" }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>zg",
+					"<cmd>Telekasten search_notes<CR>",
+					{ silent = true, desc = "Search notes using Telescope (telekasten)" }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>zd",
+					"<cmd>Telekasten goto_today<CR>",
+					{ silent = true, desc = "Go to today's daily note (telekasten)" }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>zz",
+					"<cmd>Telekasten follow_link<CR>",
+					{ silent = true, desc = "Follow link under cursor (telekasten)" }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>zn",
+					"<cmd>Telekasten new_note<CR>",
+					{ silent = true, desc = "Create new note (telekasten)" }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>zc",
+					"<cmd>Telekasten show_calendar<CR>",
+					{ silent = true, desc = "Show calendar (telekasten)" }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>zb",
+					"<cmd>Telekasten show_backlinks<CR>",
+					{ silent = true, desc = "Show backlinks (telekasten)" }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>zI",
+					"<cmd>Telekasten insert_img_link<CR>",
+					{ silent = true, desc = "Insert image link (telekasten)" }
+				)
 
-            -- Most used functions
-            vim.keymap.set(
-                "n",
-                "<leader>zf",
-                "<cmd>Telekasten find_notes<CR>",
-                { silent = true, desc = "Find notes using Telescope (telekasten)" }
-            )
-            vim.keymap.set(
-                "n",
-                "<leader>zg",
-                "<cmd>Telekasten search_notes<CR>",
-                { silent = true, desc = "Search notes using Telescope (telekasten)" }
-            )
-            vim.keymap.set(
-                "n",
-                "<leader>zd",
-                "<cmd>Telekasten goto_today<CR>",
-                { silent = true, desc = "Go to today's daily note (telekasten)" }
-            )
-            vim.keymap.set(
-                "n",
-                "<leader>zz",
-                "<cmd>Telekasten follow_link<CR>",
-                { silent = true, desc = "Follow link under cursor (telekasten)" }
-            )
-            vim.keymap.set(
-                "n",
-                "<leader>zn",
-                "<cmd>Telekasten new_note<CR>",
-                { silent = true, desc = "Create new note (telekasten)" }
-            )
-            vim.keymap.set(
-                "n",
-                "<leader>zc",
-                "<cmd>Telekasten show_calendar<CR>",
-                { silent = true, desc = "Show calendar (telekasten)" }
-            )
-            vim.keymap.set(
-                "n",
-                "<leader>zb",
-                "<cmd>Telekasten show_backlinks<CR>",
-                { silent = true, desc = "Show backlinks (telekasten)" }
-            )
-            vim.keymap.set(
-                "n",
-                "<leader>zI",
-                "<cmd>Telekasten insert_img_link<CR>",
-                { silent = true, desc = "Insert image link (telekasten)" }
-            )
-
-            -- Call insert link automatically when we start typing a link
-            vim.keymap.set(
-                "i",
-                "[[",
-                "<cmd>Telekasten insert_link<CR>",
-                { silent = true, expr = true, desc = "Insert link (telekasten)" }
-            )
-        end,
-    },
-    {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = { "markdown" },
-        build = function()
-            vim.fn["mkdp#util#install"]()
-        end,
-    },
-}
+				-- Call insert link automatically when we start typing a link
+				vim.keymap.set(
+					"i",
+					"[[",
+					"<cmd>Telekasten insert_link<CR>",
+					{ silent = true, expr = true, desc = "Insert link (telekasten)" }
+				)
+			end,
+		},
+	}
+else
+	return {}
+end
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,64 +1,91 @@
 if not vim.g.vscode then
-    return {
-        {
-            "nvim-telescope/telescope.nvim",
-            tag = "0.1.6",
-            dependencies = { "nvim-lua/plenary.nvim" },
-            config = function()
-                local builtin = require("telescope.builtin")
-                vim.keymap.set("n", "<leader>ff", builtin.find_files, {
-                    desc = "Find files using Telescope",
-                })
-                vim.keymap.set("n", "<leader>fg", builtin.live_grep, {
-                    desc = "Live grep using Telescope",
-                })
-                vim.keymap.set("n", "<leader>fb", builtin.buffers, {
-                    desc = "List open buffers using Telescope",
-                })
-                vim.keymap.set("n", "<leader>fh", builtin.help_tags, {
-                    desc = "Search help tags using Telescope",
-                })
-                vim.keymap.set("n", "<leader>fk", builtin.keymaps, {
-                    desc = "Show keymaps using Telescope",
-                })
-                vim.keymap.set("n", "<leader>fr", builtin.oldfiles, {
-                    desc = "Open recently opened files using Telescope",
-                })
+	return {
+		{
+			"nvim-telescope/telescope.nvim",
+			tag = "0.1.6",
+			dependencies = { "nvim-lua/plenary.nvim" },
+			config = function()
+				local builtin = require("telescope.builtin")
+				vim.keymap.set("n", "<leader>ff", builtin.find_files, {
+					desc = "Find files using Telescope",
+				})
+				vim.keymap.set("n", "<leader>fg", builtin.live_grep, {
+					desc = "Live grep using Telescope",
+				})
+				vim.keymap.set("n", "<leader>fb", builtin.buffers, {
+					desc = "List open buffers using Telescope",
+				})
+				vim.keymap.set("n", "<leader>fh", builtin.help_tags, {
+					desc = "Search help tags using Telescope",
+				})
+				vim.keymap.set("n", "<leader>fk", builtin.keymaps, {
+					desc = "Show keymaps using Telescope",
+				})
+				vim.keymap.set("n", "<leader>fr", builtin.oldfiles, {
+					desc = "Open recently opened files using Telescope",
+				})
 
-                local actions = require("telescope.actions")
-                require("telescope").setup({
-                    defaults = {
-                        mappings = {
-                            i = {
-                                ["<C-j>"] = actions.move_selection_next,
-                                ["<C-k>"] = actions.move_selection_previous,
-                                ["<esc>"] = actions.close,
-                            },
-                            n = {
-                                ["j"] = actions.move_selection_next,
-                                ["k"] = actions.move_selection_previous,
-                            },
-                        },
-                    },
-                })
-            end,
-        },
-        {
-            "nvim-telescope/telescope-file-browser.nvim",
-            dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-            config = function()
-                -- file browser
-                vim.keymap.set("n", "<space>fd", ":Telescope file_browser<cr>", {
-                    noremap = true,
-                    desc = "Open file browser using Telescope",
-                })
-            end,
-        },
-        {
-            "nvim-telescope/telescope-ui-select.nvim",
-            dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-        },
-    }
+				local actions = require("telescope.actions")
+				require("telescope").setup({
+					defaults = {
+						mappings = {
+							i = {
+								["<C-j>"] = actions.move_selection_next,
+								["<C-k>"] = actions.move_selection_previous,
+								["<esc>"] = actions.close,
+							},
+							n = {
+								["j"] = actions.move_selection_next,
+								["k"] = actions.move_selection_previous,
+							},
+						},
+					},
+				})
+			end,
+		},
+		{
+			"nvim-telescope/telescope-file-browser.nvim",
+			dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+			config = function()
+				-- file browser
+				vim.keymap.set("n", "<space>fd", ":Telescope file_browser<cr>", {
+					noremap = true,
+					desc = "Open file browser using Telescope",
+				})
+			end,
+		},
+		{
+			"nvim-telescope/telescope-ui-select.nvim",
+			dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+		},
+		{
+			"dharmx/telescope-media.nvim",
+			event = "VeryLazy",
+			config = function()
+				require("telescope").load_extension("media")
+				require("telescope").setup({
+					extensions = {
+						media = {
+							backend = "viu",
+							backend_options = {
+								viu = {
+									move = true,
+								},
+							},
+						},
+					},
+				})
+			end,
+		},
+		{
+			"iamcco/markdown-preview.nvim",
+			cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+			ft = { "markdown" },
+			build = function()
+				vim.fn["mkdp#util#install"]()
+			end,
+		},
+	}
 else
-    return {}
+	return {}
 end
