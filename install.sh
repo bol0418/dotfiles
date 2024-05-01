d3958eb6efb0a0cf7132e4281a3d793e1d3c2a71
bol0418 <142228386+bol0418@users.noreply.github.com>
2024-05-01 22:09:46 +0900

    update install script

--- install.sh
+++ install.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-# Dotfiles directory
-DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Array of directory names to be linked
+DIRS=("nvim" "zellij" "telekasten" ".markdownlint.json")
 
-# Create symlinks (force create, symbolic and interactive)
-ln -fsi $DOTFILES_DIR/.vimrc ~
-ln -fsi $DOTFILES_DIR/.tmux.conf ~
-ln -fsi $DOTFILES_DIR/.zshrc ~
+# Directory path for dotfiles
+DOTFILES_DIR="$HOME/dotfiles"
+
+# Iterate over each directory
+for DIR in "${DIRS[@]}"
+do
+  # Path to the configuration files for the current directory
+  CONFIG_DIR="$DOTFILES_DIR/$DIR"
+
+  # Path to the symlink destination for the current directory
+  LINK_DIR="$HOME/.config/$DIR"
+
+  # Remove the existing symlink directory if it exists
+  if [ -e "$LINK_DIR" ]; then
+    echo "Removing existing $LINK_DIR"
+    rm -rf "$LINK_DIR"
+  fi
+
+  # Create the symlink for the current directory
+  echo "Creating symlink from $CONFIG_DIR to $LINK_DIR"
+  ln -s "$CONFIG_DIR" "$LINK_DIR"
+done
+
+echo "Configuration files successfully linked!"
